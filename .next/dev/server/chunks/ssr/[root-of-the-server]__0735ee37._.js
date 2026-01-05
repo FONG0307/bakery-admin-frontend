c7510820fecc94a642c9017b7e02407b2e4a7b69
FONG0307 <236509618+FONG0307@users.noreply.github.com>
2026-01-05 14:20:40 +0700

    fix: require

--- .next/dev/server/chunks/ssr/[root-of-the-server]__0735ee37._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__0735ee37._.js
@@ -115,64 +115,11 @@ const Checkbox = ({ label, checked, id, onChange, className = "", disabled = fal
 };
 const __TURBOPACK__default__export__ = Checkbox;
 }),
-"[project]/src/components/form/input/InputField.tsx [app-ssr] (ecmascript)", ((__turbopack_context__) => {
-"use strict";
+"[project]/src/components/form/input/InputField.tsx [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
 
-__turbopack_context__.s([
-    "default",
-    ()=>__TURBOPACK__default__export__
-]);
-var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react-jsx-dev-runtime.js [app-ssr] (ecmascript)");
-;
-const Input = ({ type = "text", id, name, placeholder, defaultValue, onChange, className = "", min, max, step, disabled = false, success = false, error = false, hint })=>{
-    // Determine input styles based on state (disabled, success, error)
-    let inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${className}`;
-    // Add styles for the different states
-    if (disabled) {
-        inputClasses += ` text-gray-500 border-gray-300 cursor-not-allowed dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700`;
-    } else if (error) {
-        inputClasses += ` text-error-800 border-error-500 focus:ring-3 focus:ring-error-500/10  dark:text-error-400 dark:border-error-500`;
-    } else if (success) {
-        inputClasses += ` text-success-500 border-success-400 focus:ring-success-500/10 focus:border-success-300  dark:text-success-400 dark:border-success-500`;
-    } else {
-        inputClasses += ` bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:focus:border-brand-800`;
-    }
-    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-        className: "relative",
-        children: [
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("input", {
-                type: type,
-                id: id,
-                name: name,
-                placeholder: placeholder,
-                defaultValue: defaultValue,
-                onChange: onChange,
-                min: min,
-                max: max,
-                step: step,
-                disabled: disabled,
-                className: inputClasses
-            }, void 0, false, {
-                fileName: "[project]/src/components/form/input/InputField.tsx",
-                lineNumber: 52,
-                columnNumber: 7
-            }, ("TURBOPACK compile-time value", void 0)),
-            hint && /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
-                className: `mt-1.5 text-xs ${error ? "text-error-500" : success ? "text-success-500" : "text-gray-500"}`,
-                children: hint
-            }, void 0, false, {
-                fileName: "[project]/src/components/form/input/InputField.tsx",
-                lineNumber: 68,
-                columnNumber: 9
-            }, ("TURBOPACK compile-time value", void 0))
-        ]
-    }, void 0, true, {
-        fileName: "[project]/src/components/form/input/InputField.tsx",
-        lineNumber: 51,
-        columnNumber: 5
-    }, ("TURBOPACK compile-time value", void 0));
-};
-const __TURBOPACK__default__export__ = Input;
+const e = new Error("Could not parse module '[project]/src/components/form/input/InputField.tsx'\n\nExpected '</', got '}'");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
 }),
 "[project]/src/components/form/Label.tsx [app-ssr] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
